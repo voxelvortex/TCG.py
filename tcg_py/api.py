27a586660484cc93d6aff1efea5ec1cce5fc4de6
Matthew <34426145+MatthewCS@users.noreply.github.com>
2018-10-21 17:47:35 -0400

    Update tcg_py/api.py

--- tcg_py/api.py
+++ tcg_py/api.py
@@ -1,4 +1,32 @@
+import requests
+import json
+
 class Handler(object):
 
+    bearer = ""
+    login_data = dict()
+
     def __init__(self, **kwargs):
-        pass
+        if "bearer" in kwargs:
+            self.bearer = kwargs["bearer"]
+        elif "public" in kwargs and "private" in kwargs:
+            self._get_login_data(kwargs["public"], kwargs["private"])
+
+            if "error" in self.login_data:
+                # replace with custom exception later? :thinking:
+                raise ValueError(self.login_data["error"]
+                                 + ", description: "
+                                 + self.login_data["error_description"])
+            else:
+                self.bearer = self.login_data["access_token"]
+        else:
+            raise ValueError("Test requirers a bearer or both public"
+                             " and private arguments. You provided: "
+                             + ", ".join(kwargs.keys()))
+
+    def _get_login_data(self, public, private):
+        token_url = "https://api.tcgplayer.com/token"
+        data = "grant_type=client_credentials&client_id={0}&client_secret={1}"
+        data = data.format(public, private)
+        response = requests.request("POST", token_url, data=data)
+        self.login_data = json.loads(response.text)
